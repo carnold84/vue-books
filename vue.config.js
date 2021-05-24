4986c9f61db8e2e5a1423ea56676bfbf93ddb3e7
c-arnold <chris.wittyname@gmail.com>
2021-05-24 19:06:27 +1200

    Updated repo name

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   outputDir: "docs",
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-books-new/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-books/" : "/",
 };
